f6d0560eafd56c99fb977f9c0b3a8fc771fa3e2f
Huytran97 <t.huy@gtn-vietnam.com>
2023-07-12 15:19:33 +0700

    modified dockerfile for auto reload

---
+++ FrontEnd/app/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>about
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta odio assumenda labore dolores, pariatur voluptas amet libero alias eum corporis?</p>
+
+    </div>
+  )
+}
+
+export default about
