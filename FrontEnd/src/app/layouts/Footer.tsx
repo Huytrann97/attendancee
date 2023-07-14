40d098922d4db8ee7742115a25d8f868ccc57f8d
LPTrungGTN <l.trung@gtn-vietnam.com>
2023-07-14 10:37:58 +0700

    Footer

---
+++ FrontEnd/src/app/layouts/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="h-16 w-full flex items-center justify-center bg-gray-100">
+      <p className="text-center">GTN Vietnam (c) 2023</p>
+    </footer>
+  );
+};
+
+export default Footer;
